33e3d1f1f110676f6d9813916ea694c9b4fbc4d0
jordy muniz <jordy.muniz96@gmail.com>
2021-09-18 21:48:20 -0500

    added home page and part of register.js

---
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar(props) {
+    return `
+        <nav>
+            <a href="/" data-link>Home</a>
+            <a href="/posts" data-link>Posts</a>
+            <a href="/about" data-link>About</a>
+            <a href="/login" data-link>Login</a>
+            <a href="/register" data-link>Register</a>
+            <a href="/users" data-link>Users</a>
+           
+        </nav>
+    `;
+}
